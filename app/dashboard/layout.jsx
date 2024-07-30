653445ec0db828d81136e022b19349b220c51b71
mucahitkarakus <mocococo.80@gmail.com>
2024-07-30 13:09:42 +0300

    layout dashboard ui

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,14 +1,17 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
+import styles from "../ui/dashboard/dashboard.module.css"
 
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
   return (
-    <div>
-        <div>
-        <Sidebar />
+    <div className={styles.container}>
+        <div className={styles.menu}>
+            <Sidebar />
         </div>
-        <Navbar />
-        { children }
+        <div className={styles.content}>
+            <Navbar />
+            {children}
+        </div>
     </div>
   )
 }
