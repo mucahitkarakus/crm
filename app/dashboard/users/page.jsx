51f4020e3dd59c8dcb272b697af62ea3b6217e6b
mucahitkarakus <mocococo.80@gmail.com>
2024-07-30 14:31:55 +0300

    users section done

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import styles from "@/app/ui/dashboard/users/users.module.css"
 import Search from "@/app/ui/dashboard/search/search"
+import Image from "next/image"
 
 const ProductsPage = async () => {
   return (
@@ -11,6 +12,133 @@ const ProductsPage = async () => {
           <button className={styles.addButton}>Add New</button>
         </Link>
       </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  width={40}
+                  height={40}
+                  alt="User Avatar"
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>johndoe@gmail.com</td>
+            <td>15 Agu 2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+             <td>
+                <div className={styles.buttons}>
+                  <Link href="/dashboard/user">
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  width={40}
+                  height={40}
+                  alt="User Avatar"
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>johndoe@gmail.com</td>
+            <td>15 Agu 2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+             <td>
+                <div className={styles.buttons}>
+                  <Link href="/dashboard/user">
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  width={40}
+                  height={40}
+                  alt="User Avatar"
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>johndoe@gmail.com</td>
+            <td>15 Agu 2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+             <td>
+                <div className={styles.buttons}>
+                  <Link href="/dashboard/user">
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  width={40}
+                  height={40}
+                  alt="User Avatar"
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>johndoe@gmail.com</td>
+            <td>15 Agu 2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+             <td>
+                <div className={styles.buttons}>
+                  <Link href="/dashboard/user">
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+          </tr>
+        </tbody>
+
+      </table>
     </div>
   )
 }
