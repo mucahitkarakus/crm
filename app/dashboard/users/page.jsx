926e32aada00c65be52ae49a62204e94ca43185f
mucahitkarakus <mocococo.80@gmail.com>
2024-07-30 14:24:52 +0300

    search comps done for users and products section

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,18 @@
-const UsersPage = () => {
+import Link from "next/link"
+import styles from "@/app/ui/dashboard/users/users.module.css"
+import Search from "@/app/ui/dashboard/search/search"
+
+const ProductsPage = async () => {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for user..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
-export default UsersPage
+export default ProductsPage
